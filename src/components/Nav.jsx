04e20b67b3dd256108eeca0264641b527d2131e9
connor <connoroakvillea32@hotmail.com>
2023-09-30 20:06:45 -0400

    nav bar done for now

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ function Nav() {
         <div className="flex items-center justify-between">
           <a className="flex-none text-xl font-semibold dark:text-white" href="#">Focus</a>
           <div className="sm:hidden">
-            <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
+            <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
               <svg className="hs-collapse-open:hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
               </svg>
@@ -22,7 +22,7 @@ function Nav() {
             <a className="font-medium text-white/70 hover:text-white" href="#">Account</a>
             <a className="font-medium text-white/70 hover:text-white" href="#">Work</a>
             <a className="font-medium text-white/70 hover:text-white" href="#">Blog</a>
-            <p className="text-white/70">|</p>
+            <p className="hidden sm:block text-white/70">|</p>
             <a className="font-medium text-white/70 hover:text-white" href="#">Log in</a>
             <a className="font-medium text-white/70 hover:text-white" href="#">Sign up</a>
           </div>
